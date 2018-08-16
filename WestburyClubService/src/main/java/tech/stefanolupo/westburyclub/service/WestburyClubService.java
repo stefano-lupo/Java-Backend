7ed8e52f4c586906b4297bf634fbaf5df90d2715
Stefano <lupos@tcd.ie>
2018-08-16 11:22:42 +0100

    CORS stuff

--- WestburyClubService/src/main/java/tech/stefanolupo/westburyclub/service/WestburyClubService.java
+++ WestburyClubService/src/main/java/tech/stefanolupo/westburyclub/service/WestburyClubService.java
@@ -4,7 +4,12 @@ import com.hubspot.dropwizard.guice.GuiceBundle;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import tech.stefanolupo.westburyclub.service.config.WestburyClubServiceModule;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class WestburyClubService extends Application<WestburyClubServiceConfiguration> {
 
@@ -33,5 +38,18 @@ public class WestburyClubService extends Application<WestburyClubServiceConfigur
     public void run(WestburyClubServiceConfiguration configuration, Environment environment) throws Exception {
         // Auto config handles resources
         // health checks must not inherit from InjectableHealthCheck
+        // Enable CORS headers
+
+        //TODO
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
     }
 }
